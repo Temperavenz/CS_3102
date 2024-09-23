3984aedc7d35d753eec0c869b430543ea474589d
Temperavenz <22103492@usc.edu.ph>
2024-09-24 00:08:56 +0800

    Initial commit: Template for the 3 infamous sorting algos

---
+++ Sorting Algos/Header_files/header.h
@@ -0,0 +1,11 @@
+#ifndef SORTING_HEADER
+#define SORTING_HEADER
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define ARR_SIZE 10
+typedef int Data[ARR_SIZE];
+
+#endif
---
+++ Sorting Algos/insertionSort.c
@@ -0,0 +1,23 @@
+#include "Header_files/header.h"
+
+void insertionSort(int *arr, int size);
+void display(int *arr, int size);
+
+int main(void) {
+    return 0;
+}
+
+void insertionSort(int *arr, int size) {
+}
+
+void display(int *arr, int size) {
+    int x;
+    printf("{");
+    for(x=0; x<size; ++x) {
+        printf("%d", arr[x]);
+        if(x<size-1) {
+            printf(", ");
+        }
+    }
+    puts("}");
+}
