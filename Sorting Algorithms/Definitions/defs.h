e4e3b0cd2ac35c8dda90cee81d208de6ab31f462
Temperavenz <22103492@usc.edu.ph>
2024-10-12 18:34:28 +0800

    Sorting Algos: Added the diff algos' templates for its prototypes and definitions

--- Sorting Algorithms/Definitions/defs.h
+++ Sorting Algorithms/Definitions/defs.h
@@ -3,4 +3,99 @@
 
 #include "../Prototypes/protos.h"
 
+/********************************
+*   Part A Function Definitions *
+*********************************/
+void bubbleSort(int arr[], int size) {
+    // Insert code here
+}
+
+void insertionSort(int arr[], int size) {
+    // Insert code here
+}
+
+void selectionSort(int arr[], int size) {
+    // Insert code here
+}
+
+void shellSort(int arr[], int size) {
+    // Insert code here
+}
+
+void combSort(int arr[], int size) {
+    // Insert code here
+}
+
+void heapSort(int arr[], int size) {
+    // Insert code here
+}
+void maxHeapify(int arr[], int node) {
+    // Insert code here
+}
+void deleteMax(int arr[], int node) {
+    // Insert code here
+}
+
+void tournamentSort(int arr[], int size) {
+    // Insert code here
+}
+
+/********************************
+*   Part B Function Definitions *
+*********************************/
+void gnomeSort(int arr[], int size) {
+    // Insert code here
+}
+
+void countingSort(int arr[], int size) {
+    // Insert code here
+}
+
+void radixSort(int arr[], int size) {
+    // Insert code here
+}
+void countingRadix(int arr[], int size) {
+    // Insert code here
+}
+
+void mergeSort(int arr[], int leftIdx, int rightIdx) {
+    // Insert code here
+}
+void merge(int arr[], int leftIdx, int midIdx, int rightIdx) {
+    // Insert code here
+}
+
+void quickSortLomuto(int arr[], int low, int high) {
+    // Insert code here
+}
+void lomutoPartition(int arr[], int low, int high) {
+    // Insert code here
+}
+
+void quickSortHoare(int arr[], int low, int high) {
+    // Insert code here
+}
+void hoarePartition(int arr[], int low, int high) {
+    // Insert code here
+}
+
+void bucketSort(float arr[], int size) {
+    // Insert code here
+}
+void insertSorted(List *buckets, float elem) {
+    // Insert code here
+}
+
+void strandSort(int ipArr[], int ipSize, int opArr[], int *opSize) {
+    // Insert code here
+}
+void strandMerge(int tempArr[], int tempSize, int sublist[], int subSize, int opArr[]) {
+    // Insert code here
+}
+
+// Display function definition
+void displayArr(int arr[], int size) {
+    // Insert code here
+}
+
 #endif
--- Sorting Algorithms/Prototypes/protos.h
+++ Sorting Algorithms/Prototypes/protos.h
@@ -6,4 +6,59 @@
 #include <string.h>
 #include <stdbool.h>
 
+/************************
+*   Linked-List Build   *
+*************************/
+typedef struct node {
+    float data;
+    struct node *link;
+}*List;
+
+/********************************
+*   Part A Function Prototypes  *
+*********************************/
+void bubbleSort(int arr[], int size);
+
+void insertionSort(int arr[], int size);
+
+void selectionSort(int arr[], int size);
+
+void shellSort(int arr[], int size);
+
+void combSort(int arr[], int size);
+
+void heapSort(int arr[], int size);
+void maxHeapify(int arr[], int node);
+void deleteMax(int arr[], int node);
+
+void tournamentSort(int arr[], int size);
+
+/********************************
+*   Part B Function Prototypes  *
+*********************************/
+void gnomeSort(int arr[], int size);
+
+void countingSort(int arr[], int size);
+
+void radixSort(int arr[], int size);
+void countingRadix(int arr[], int size);
+
+void mergeSort(int arr[], int leftIdx, int rightIdx);
+void merge(int arr[], int leftIdx, int midIdx, int rightIdx);
+
+void quickSortLomuto(int arr[], int low, int high);
+void lomutoPartition(int arr[], int low, int high);
+
+void quickSortHoare(int arr[], int low, int high);
+void hoarePartition(int arr[], int low, int high);
+
+void bucketSort(float arr[], int size);
+void insertSorted(List *buckets, float elem);
+
+void strandSort(int ipArr[], int ipSize, int opArr[], int *opSize);
+void strandMerge(int tempArr[], int tempSize, int sublist[], int subSize, int opArr[]);
+
+// Display function prototype
+void displayArr(int arr[], int size);
+
 #endif
